70e4152d1424c43e47d9170adfa73f3f0175f9c2
Xiexiaofei <505218757@qq.com>
2020-02-17 15:21:32 +0800

    crossover_solution_one

---
+++ CrossOverMinDiffDisProblem/CrossOver.h
@@ -0,0 +1,75 @@
+#pragma once
+#ifndef MIN_DIFF_DP_LOCALSEARCH_H
+#define MIN_DIFF_DP_LOACLSEARCH_H
+#include "InputOutput.h"
+
+using namespace std;
+
+namespace min_diff_dp {
+
+    class CrossOver {
+    public:
+        CrossOver(const UMatrix &_matrix, int _tabu_iter, int _tabu_length, int popu, double param, const Solution &_init_sol_one, const Solution &_init_sol_two,
+            const int _size_of_tabu, const double _param1, const double _param2, const double _param3);
+        Solution solve();
+    private:
+        void init();
+        void cross();
+        void tabusearch();
+        void update();
+    private:
+        void find_best_move(const int number, pair<int, int> &_pair, pair<Distance, Distance> &_new_obj, int &_hash_one, int &_hash_two, int &_hash_three);
+        bool update_solu(const int number, const pair<int, int> &_pair, const pair<Distance, Distance> &_new_obj, int &_hash_one, int &_hash_two, int &_hash_three, int &step);
+
+    private:
+        const UMatrix &ins;      //算例矩阵
+
+        List<int> cross_best;
+        Distance cross_best_obj;
+
+
+        List<int> tabu_list_one;                   //三个禁忌列表
+        List<int> tabu_list_two;
+        List<int> tabu_list_three;
+        List<int> hash_key_temp_one;          //保存所有节点的hash中间键值(int)(floor(pow(i, hashFun_one_param)))
+        List<int> hash_key_temp_two;
+        List<int> hash_key_temp_three;
+
+        int nb_nodes;                   //图中节点数目
+        int nb_sub_nodes;               //所选的节点数目
+
+        int population;               //种群个数
+        int single_iter_length;                      //禁忌搜索长度
+        int tabu_iter_max;                     //禁忌搜索的最大迭代次数
+        int generation;                      //交叉次数
+        int size_neighbor_struc;          //参与邻域动作的I0元素个数
+
+        long long max_time;             //迭代运行的最长时间=算例的节点数目
+        double rate_of_sele_nodes;        //从I0中选择邻域动作的大小比例
+        int size_of_tabu_list;        //L大小
+        double hashFun_one_param;     //三个哈希函数的参数
+        double hashFun_two_param;
+        double hashFun_three_param;
+
+        List<List<int>> node_value;    //当前解        ？（是用List保存，还是Set保存；更快）
+        List<Distance> cur_obj;        //当前目标
+        List<List<int>> local_best;     //局部最优解
+        List<Distance> local_best_obj;        //局部最优目标函数
+
+        List<List<Distance>> node_dis_sum;     //每个节点与选中集合中节点的距离和--邻域结构
+        List<Distance> max_select_node;                  //记录node_dis_sum中距离最大值的选中节点
+        List<Distance> min_select_node;                  //记录node_dis_sum中距离最小值的选中节点
+                                     //保存历史最优解的相关数据；强化搜索策略（S<-历史最优解）中方便使用
+        List<List<Distance>> best_solu_dis_sum;
+        List<Distance> best_max_select_node;
+        List<Distance> best_min_select_node;
+
+        List<List<Pair<int, Distance>>> no_select_nodes;         //未选中的节点排序--辅助结构,Dis-(max+min)/2
+        List<List<Pair<int, Distance>>> select_nodes;             //选中的节点排序--辅助结构
+
+        List<int> best_hashfun_one;      //中间值:历史最优解的三个哈希函数值
+        List<int> best_hashfun_two;
+        List<int> best_hashfun_three;
+    };
+}
+#endif
--- CrossOverMinDiffDisProblem/main.cpp
+++ CrossOverMinDiffDisProblem/main.cpp
@@ -3,7 +3,7 @@
 #include<sstream>
 #include "utility.h"
 #include "InputOutput.h"
-#include "LocalSearch.h"
+#include "CrossOver.h"
 
 using namespace std;
 using namespace xxf_utility;
@@ -13,25 +13,31 @@ void test_localSearch(String &filename) {
     //TODO：各个参数如何更方便(_param和tabuStep是根据算例修改的)
     const double _param = 0.3;           //表示邻域大小的参数
     const int tabuStep = 35;
+    const int tabu_max_length = 2000;
+    const int population = 2;
     const int sizeTabu = 100000000;
     const double param1 = 1.8;
     const double param2 = 1.9;
     const double param3 = 2.0;
     UMatrix matrix(filename);
-    Solution sl(matrix.setele_num(), matrix.subsetele_num());
-    //time_t t = time(0);
-    //myrand.setSeed(t);
+    Solution sl_one(matrix.setele_num(), matrix.subsetele_num());
+    Solution sl_two(matrix.setele_num(), matrix.subsetele_num());
+    time_t t = time(0);
+    myrand.setSeed(t);
+    mylog << "1.随机种子为：" << t <<= logsw_info;
+    sl_one.randomInit();
     //test
-    time_t t = 1581402801;
-    myrand.setSeed(1581402801);
+    //time_t t = 1581674941;
+    //myrand.setSeed(1581674941);
     //test end
-    sl.randomInit();
-    mylog << "随机种子为：" << t <<= logsw_info;
-    mylog << "总节点数：" << sl.get_nb_nodes()
-        <<"挑选的节点数："<< sl.get_sub_nb_nodes() <<= logsw_info;
-    LocalSearch local(matrix, _param, sl, tabuStep, sizeTabu, param1, param2, param3);
-    //Solution sol = local.solve_tabu();
-    Solution sol = local.solve();
+    time_t t1 = t - rand() % 1000000000;
+    myrand.setSeed(t1);
+    mylog << "2.随机种子为：" << t1 <<= logsw_info;
+    sl_two.randomInit();
+    mylog << "总节点数：" << sl_one.get_nb_nodes()
+        <<"挑选的节点数："<< sl_two.get_sub_nb_nodes() <<= logsw_info;
+    CrossOver crossover(matrix, tabuStep, tabu_max_length, population, _param, sl_one, sl_two, sizeTabu, param1, param2, param3);
+    Solution sol = crossover.solve();
     if (!sol.check(matrix))mylog << "目标函数值冲突" <<= logsw_error;
     sol.print();
 }
@@ -39,58 +45,41 @@ void test_localSearch(String &filename) {
 void benchmark(void test(string&)) {
     constexpr int max_num_calculations = 3;         //最大的计算次数
     List<string> instances_id{
-        //"a_1_n500_m50"
-        //,"a_2_n500_m50",
-        //"a_3_n500_m50","a_4_n500_m50",
-        //"a_5_n500_m50",
-        //"a_21_n2000_m200"
-        //"a_22_n2000_m200","a_23_n2000_m200","a_24_n2000_m200","a_25_n2000_m200",
-        //"a_1_n500_m50",
-        //"a_2_n500_m50",
-        //"a_9_n500_m50"
-        //"a_10_n500_m50",
-        //"a_17_n500_m50",
-        //"a_19_n500_m50",
-        //"a_20_n500_m50",
-        "a_21_n2000_m200"
-        //"a_22_n2000_m200",
-        //"a_27_n2000_m200",
-        //"a_28_n2000_m200",
-        //"a_30_n2000_m200",
-        //"a_33_n2000_m200",
-        //"a_35_n2000_m200",
-        //"a_39_n2000_m200",
-        //"a_40_n2000_m200",
-        //"b_1_n500_m50",
-        //"b_2_n500_m50",
-        //"b_9_n500_m50",
-        //"b_10_n500_m50",
-        //"b_17_n500_m50",
-        //"b_19_n500_m50",
-        //"b_20_n500_m50",
-        //"b_21_n2000_m200",
-        //"b_22_n2000_m200",
-        //"b_27_n2000_m200",
-        //"b_28_n2000_m200",
-        //"b_30_n2000_m200",
-        //"b_33_n2000_m200",
-        //"b_35_n2000_m200",
-        //"b_39_n2000_m200",
-        //"b_40_n2000_m200",
-        // "c_1_n3000_m300",
-        //"c_3_n3000_m300",
-        //"c_9_n3000_m300"
-        //"c_12_n3000_m300"
-        //"c_14_n3000_m300",
-        //"c_19_n3000_m300",
-        //"c_20_n3000_m300"
+        //MDG-a:算例
+        "MDG-a_1_n500_m50",
+        "MDG-a_7_n500_m50",
+        "MDG-a_13_n500_m50",
+        "MDG-a_18_n500_m50",
+        "MDG-a_25_n2000_m200",
+        "MDG-a_33_n2000_m200",
+        //MDG-b算例
+        "MDG-b_3_n500_m50",
+        "MDG-b_10_n500_m50",
+        "MDG-b_12_n500_m50",
+        "MDG-b_19_n500_m50",
+        "MDG-b_28_n2000_m200",
+        "MDG-b_33_n2000_m200",
+        //MDG-C算例
+         "MDG-c_3_n3000_m300",
+         "MDG-c_7_n3000_m400",
+         "MDG-c_11_n3000_m500",
+         "MDG-c_19_n3000_m600",
+         //SOM-b算例
+         "SOM-b_2_n100_m20",
+         "SOM-b_4_n100_m40",
+         "SOM-b_5_n200_m20",
+         "SOM-b_8_n200_m80",
+         "SOM-b_9_n300_m30",
+         "SOM-b_12_n300_m120",
+         "SOM-b_13_n400_m40",
+         "SOM-b_16_n400_m160",
     };
     for (int i = 0; i < instances_id.size(); ++i) {
         int count = 0;
         for (int c_time = 0; c_time < max_num_calculations; ++c_time) {
             //string file_name = "Instances/MDG-" + instances_id[i] + ".txt";
             //自己电脑-实验室电脑 区别
-            string file_name = "../Deploy/Instances/MDG-" + instances_id[i] + ".txt";
+            string file_name = "../Deploy/Instances/" + instances_id[i] + ".txt";
             ++count;
             mylog << "第" << count << "次测试算例" << instances_id[i] << "" <<= LogSwitch(1, 1, "BenchMark");
             test(file_name);
